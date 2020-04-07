3e1b3998a299c39a6f7eea9e2546e0e4e1ae60d8
github username <github email address>
2020-04-07 16:56:47 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,21 +1,75 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
+// {
+//   "height": 10,
+//   "weight": 130,
+//   "id": 2,
+//   "name": "ivysaur",
+//   "abilities": ["overgrow", "chlorophyll"],
+//   "moves": [],
+//   "stats": [
+//     {
+//       "value": 80,
+//       "name": "special-defense"
+//     },
+//     {
+//       "value": 80,
+//       "name": "special-attack"
+//     },
+//     {
+//       "value": 63,
+//       "name": "defense"
+//     },
+//     {
+//       "value": 62,
+//       "name": "attack"
+//     },
+//     {
+//       "value": 60,
+//       "name": "speed"
+//     },
+//     {
+//       "value": 60,
+//       "name": "hp"
+//     }
+//   ],
+//   "types": ["grass", "poison"],
+//   "sprites": {
+//     "front": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+//     "back":
+//       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png"
+//   }
+// },
+
 class PokemonCard extends React.Component {
+
+  state={
+    imageDisplayed:this.props.pokemon.sprites.front
+  }
+
+  showBackImg = () => {
+    this.setState(prevState => {
+      const imageDisplayed = prevState.imageDisplayed === this.props.pokemon.sprites.front ? this.props.pokemon.sprites.back : this.props.pokemon.sprites.front;
+      return {imageDisplayed}
+    })
+  }
+
   render() {
+    const pokemon = this.props.pokemon;
     return (
-      <Card>
+      <Card onClick={this.showBackImg}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.imageDisplayed} alt={pokemon.name} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {pokemon.stats[5].value}
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,78 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      pokemons: [],
+      displayPokemons: [],
+      searchPoke: "",
+      newId: 208
+    }
+    this.pokeUrl = 'http://localhost:3000/pokemon';
+  }
+
+  componentDidMount() {
+    fetch(this.pokeUrl)
+      .then(res => res.json())
+      .then(pokemons => this.setState({pokemons, displayPokemons:pokemons}))
+      .catch(err => console.log(err))
+  }
+
+  handleChange = (value) => {
+    this.setState(() => {
+      const searchPoke = value;
+      const displayPokemons = this.state.pokemons.filter(pokemon => pokemon.name.toLowerCase().indexOf(searchPoke) >= 0);
+      console.log(displayPokemons)
+      return {searchPoke, displayPokemons}
+    })
+  }
+
+  addPokemon = (event, pokemon) => {
+    event.preventDefault();
+    ;
+    const postObj = {
+      'method': 'POST',
+      'headers': {
+        'Content-Type':'application/json',
+        'Accept': 'application/json'
+      },
+      'body': JSON.stringify({
+        'id': this.state.newId,
+        'name':pokemon.name, 
+        'stats': [{},{},{},{},{},{'value':pokemon.hp}],
+        'sprites': {'front': pokemon.frontUrl, 'back': pokemon.backUrl}
+      })
+    };
+
+    fetch(this.pokeUrl, postObj)
+      .then(res => res.json())
+      .then(newPoke => {
+        this.setState({
+          pokemons:[
+            ...this.state.pokemons,
+            newPoke
+          ],
+          displayPokemons: [
+            ...this.state.pokemons,
+            newPoke
+          ],
+          newId: this.state.newId + 1
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.addPokemon}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={this.handleChange} searchPoke={this.state.searchPoke}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.displayPokemons}/>
       </Container>
     )
   }
