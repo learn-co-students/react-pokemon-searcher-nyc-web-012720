247bf4cbc9533f5332b226cf58cadfeb92f1f6e3
mdiaz26 <mike.diaz006@gmail.com>
2020-04-07 21:02:31 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,31 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  
+  state = {
+    showBack: false
+  }
+  
+  clickHandler = () => {
+    this.setState(prevState => ({showBack: !prevState.showBack}))
+  }
+
   render() {
+    const {name, stats, sprites} = this.props.data
+    const hp = stats.find(stat => stat.name === "hp")
     return (
-      <Card>
+      <Card onClick={this.clickHandler}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.showBack ? sprites.back : sprites.front} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp.value}
             </span>
           </div>
         </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Form, Radio } from 'semantic-ui-react'
 
 const Search = props => {
   return (
-    <div className="ui search">
-      <div className="ui icon input">
-        <input className="prompt" onChange={props.onChange} />
-        <i className="search icon" />
+    <div>
+      <Form>
+        <Form.Field>
+          Selected value: <b>{props.searchBy}</b>
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='Name'
+            name='radioGroup'
+            value='name'
+            checked={props.searchBy === 'name'}
+            onChange={props.handleRadioChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='Type'
+            name='radioGroup'
+            value='type'
+            checked={props.searchBy === 'type'}
+            onChange={props.handleRadioChange}
+          />
+        </Form.Field>
+      </Form>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input className="prompt" onChange={props.onChange} value={props.value} placeholder="search by name"/>
+          <i className="search icon" />
+        </div>
       </div>
     </div>
   )
